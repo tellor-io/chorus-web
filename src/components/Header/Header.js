058251b966ea6c129ec39e211ded3287f3bc8c8d
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2021-01-18 21:32:44 +0200

    fix some linting errors
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './Header.scss';
 import Icons from '../../Icons';
-import { Button } from 'antd';
 import ChorusButton from '../ChorusButton/ChorusButton';
 import { useMediaQuery } from 'react-responsive';
 
 function Header() {
-  const isMobile = useMediaQuery({query: '(max-width: 810px)'})
+  const isMobile = useMediaQuery({ query: '(max-width: 810px)' })
 
   return (
     <div className="Header">
-      <Icons.Logo className="HeaderLogo" width={isMobile?"72px":"82px"} />
+      <Icons.Logo className="HeaderLogo" width={isMobile ? "72px" : "82px"} />
       <h1>Write an Anthem.</h1>
       <p className="HeaderIntro"><span className="bold">Chorus</span> is a structure for enabling anyone to create and issue semi-stablecoins on Ethereum for use in their own ecosystem.</p>
       <ChorusButton txt="Read the whitepaper" />
